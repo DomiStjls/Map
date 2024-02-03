e9efc65330ad9280131314ed28b985af2eccd682
MaksimKharlamov <101044403+MaksimKharlamov@users.noreply.github.com>
2024-02-03 17:10:02 +0300

    задача 1

--- test3.py
+++ test3.py
@@ -1,18 +1,39 @@
+from io import BytesIO
+
+import requests
 from PyQt5 import QtWidgets, uic, QtCore
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QPixmap
 import sys
+from PIL import Image
+
+
+def get_image(coor, delta):
+    lattitude, longitude = coor.split(" ")
+    map_params = {
+        "ll": ",".join([longitude, lattitude]),
+        "spn": ",".join([delta, delta]),
+        "l": "sat"
+    }
+
+    map_api_server = "http://static-maps.yandex.ru/1.x/"
+    response = requests.get(map_api_server, params=map_params)
+
+    Image.open(BytesIO(
+        response.content)).save("ans.jpg")
+
 
 class Map(QtWidgets.QMainWindow):
     def __init__(self):
         super(Map, self).__init__()
         uic.loadUi("map.ui", self)
-        pixmap = QPixmap("ans.png")
+        pixmap = QPixmap("ans.jpg")
         self.image.setPixmap(pixmap)
 
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
+    get_image("59.119361 37.904043", "0.005")
     main = Map()
     main.show()
     sys.exit(app.exec_())
